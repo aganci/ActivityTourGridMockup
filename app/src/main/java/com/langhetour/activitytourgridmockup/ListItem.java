4aff91ab2eaa62a87e61d3f07ac9f76db264fddc
aganci <aganci@gmail.com>
2017-04-25 21:03:34 +0200

    change gradient

--- app/src/main/java/com/langhetour/activitytourgridmockup/ListItem.java
+++ app/src/main/java/com/langhetour/activitytourgridmockup/ListItem.java
@@ -14,13 +14,17 @@ public class ListItem {
         result.add(new Breakfast("Dolce Langa", R.drawable.pasticceria_la_dolce_langa));
         result.add(new CategoryItem("Visitare"));
         result.add(new Breakfast("Cascina Barroero", R.drawable.cascina_barroero));
-        result.add(new Breakfast("Castello Monesiglio", R.drawable.castello_monesiglio));
-        result.add(new Breakfast("Castello Prunetto", R.drawable.castello_prunetto));
-        result.add(new Breakfast("Castello Saliceto", R.drawable.castello_saliceto));
+        result.add(new Breakfast("Pieve di S. Maria", R.drawable.pieve_di_santa_maria_cortemilia));
         result.add(new Breakfast("Piazza Molinari", R.drawable.piazza_molinari_cortemilia));
+        result.add(new Breakfast("Centro del Mondo", R.drawable.monumento_centro_del_mondo_cortemilia));
+        result.add(new Breakfast("Bergolo", R.drawable.bergolo));
+        result.add(new Breakfast("Castello Prunetto", R.drawable.castello_prunetto));
         result.add(new CategoryItem("Mangiare"));
-        result.add(new Breakfast("Langhet", R.drawable.langhet));
+        result.add(new Breakfast("Osteria Langhet", R.drawable.langhet));
         result.add(new Breakfast("Pizzeria La Torre", R.drawable.pizzeria_la_torre_cortemilia));
+        result.add(new CategoryItem("Dormire"));
+        result.add(new Breakfast("Orchidea Selvatica", R.drawable.b_b_orchidea_selvatica));
+        result.add(new Breakfast("Le Coccinelle", R.drawable.b_b_coccinelle));
         return result;
     }
 
